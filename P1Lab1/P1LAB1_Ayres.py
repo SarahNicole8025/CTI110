f554f3ff33fdd53a4cdffbc8486dc7279c331a65
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-01 13:30:32 -0400

    Add files via upload

---
+++ P1Lab1/P1LAB1_Ayres.py
@@ -0,0 +1,40 @@
+# CTI 110
+#P1LAB1 - MY First Program
+#Ayress
+#9/24/24
+'''
+print("Hello world!")
+#geet the user by name
+#TODO: ask their name
+print("this is a python program")
+print('Whats your name')
+
+#Eample of formatting
+#Comments are # or triple quotes
+
+print(' The cow says"Moo"')
+print("Newline\n is kind of like \n<br>\n")
+print("\tTabs space out\t\tword")
+print("\\ is the backslash")
+
+print(' The cow says"Moo"')
+print("Newline\n is kind of like \n<br>\n")
+print("\tTabs space out\t\tword")
+print("\\ is the backslash")
+'''
+print('Hello')
+
+#Greet the user ny name
+#TODO: ask their name
+print(" what's your name?")
+print ('Enter first name:')
+first = input()
+print('Enter last name:')
+last = input()
+
+#say their name
+print("Nice to meet you, ",first,last)
+       
+# ask them if they like stuff
+#print("What do you like?")
+topic = input("What do you like?")
