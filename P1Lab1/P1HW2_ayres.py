861765f1939f4ce9520cd4779b7088742777db9b
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-08 18:04:41 +0000

    P1HW2

---
+++ P1Lab1/P1HW2_ayres.py
@@ -0,0 +1,37 @@
+#CTI 110
+#P1HW2
+#S Ayres
+#10/8/2024
+'''
+3. Ask user to enter their budget
+
+4. Ask user to enter travel destination
+
+5.  Ask user for amount they will spend on gas
+
+6. Ask user for amount they will spend on accommodation
+
+7. Ask user for amount they will spend on food
+
+8. Add expenses
+
+9. Subtract expenses from budget
+
+10. Display results
+'''
+print("Your Budget: $", format(bus))
+print("-----Trip Calculator-----")
+#Ask user to enter their budget
+budget = float(input("What is your travel budget? "))
+#Ask user to enter travel destination
+destination = input("Where are you going? ")
+# Ask user for amount they will spend on gas
+gas = float(input("How amount for gas? "))
+#Ask user for amount they will spend on accommodation
+hotel = float(input("Lodging budget? "))
+#Ask user for amount they will spend on food
+food = float (input("Food Budget: "))
+#Add expenses
+extras = float(input("How much to alot for extras? "))
+#Subtract expenses from budget
+print(used = int(destination + gas + hotel + food + extras - budget))
