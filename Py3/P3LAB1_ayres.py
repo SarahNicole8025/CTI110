0fd685b6a56d8927dd7d002eda97481eaa195085
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-22 17:50:23 +0000

    P3L1 edit 1.1

--- Py3/P3LAB1_ayres.py
+++ Py3/P3LAB1_ayres.py
@@ -3,4 +3,42 @@
 # Ayress
 # 10-22-24
 
-print("Hello World")
+def main():
+    print("Choose you own Adventure")
+    go_home()
+
+def go_home():
+    print("You decide to go Home.")
+    print("But should you order some takeout?")
+    print("1.chinese")
+    print("2.thai")
+    choice = int(input())
+    if choice == 1:
+        get_chinese()
+    elif choice == 2:
+        get_thai()
+
+def  get_chinese():
+    print("You go looking for your chinese takeout booklet")
+    print("You find 3 places that has what you want.")
+    print("1.Dragon Tails")
+    print("2.Luckys")
+    print("3.Zou's Place")
+    choice = int(input())
+    if choice == 1:
+        get_Dragon_()
+    elif choice == 2:
+        get_luckys()
+    elif choice == 3:
+        get_zou_place()
+
+     # replace
+
+def get_thai():
+    print("You look up tai places near you")
+    print("There are only 2 places n your town.")
+    print("1.Delivers but is two stars.")
+    print("2. Is only carryout but is 5 stars.")
+
+#Start Program
+main()
