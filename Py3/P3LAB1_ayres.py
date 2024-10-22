fb1017b566d17143bb30ccb0ae1520767866c936
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-22 18:04:53 +0000

    P3L1 Edit 1.2

--- Py3/P3LAB1_ayres.py
+++ Py3/P3LAB1_ayres.py
@@ -5,17 +5,35 @@
 
 def main():
     print("Choose you own Adventure")
+    print("Your friends have invited you to explore an abbandden Insane Asythem")
+    go_Asythem()
     go_home()
+
+def go_Asythem():
+    print("You are dared to go in alone")
+    print("You go in as groups of two")
+    choice = input()
+    if choice == alone:
+        go_alone
+    elif choice == group:
+        go_in_group
+
+def go_alone():
+    pass
+
+def go_in_group():
+    pass
+
 
 def go_home():
     print("You decide to go Home.")
     print("But should you order some takeout?")
-    print("1.chinese")
-    print("2.thai")
-    choice = int(input())
-    if choice == 1:
+    print("chinese")
+    print("thai")
+    choice = input()
+    if choice == chinese:
         get_chinese()
-    elif choice == 2:
+    elif choice == thai:
         get_thai()
 
 def  get_chinese():
@@ -38,7 +56,12 @@ def get_thai():
     print("You look up tai places near you")
     print("There are only 2 places n your town.")
     print("1.Delivers but is two stars.")
-    print("2. Is only carryout but is 5 stars.")
+    print("2.Is only carryout but is 5 stars.")
+    choice = int(input())
+    if choice == 1:
+        get_delivery()
+    elif choice == 2:
+        get_carryout()
 
 #Start Program
 main()
