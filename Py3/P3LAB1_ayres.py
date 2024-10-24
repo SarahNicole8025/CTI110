c90d02ce48d7f285114b84c47c1e74baa1973a9d
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-24 18:04:06 +0000

    Admins 1.1

--- Py3/P3LAB1_ayres.py
+++ Py3/P3LAB1_ayres.py
@@ -24,6 +24,63 @@ def stay_home():
     print("relieved to be in your safe space.")
     print("Yay! Your Safe, but you find out in the morning on the news that they were all killed by a ")
     print("axe whilding Masked figure that has been terrizing the town as of late.")
+def go_asylum():
+    print("As you and your friends stand beforw the imposing gates of Blackwood Asylum. the air")
+    print("is thick with anticipation and an unsetting chill. You can't shake teh feeling something is")
+    print("waitig. As your friends buzz with exitemen, urging you to be the brave one, you feel a mixture")
+    print("of anixety and thri;;. You glance back at them,their faes a mix of aniticipation and concern")
+    print("Are you willing to venture into the asylum alone, relying on your instincts, or would you prefer")
+    print("to explore it with a group")
+    print("[Alone]or[Group]")
+    choice = input()
+    if choice == "group":
+        go_group
+    elif choice == "alone":
+        go_alone
+def go_group():
+    print("One of your other friends steps forward, concern etched on their face.'No way! we can't send you")
+    print("in alone. What id somethinf happens to you?'The atmosphere thickens with tension. you take a deep breath,")
+    print("feeling the weight o thier expectations and fears. Finally, you nod in agreement.Thank god someone said")
+    print("so i didn't have to and sound like a coward you think to yourself. As you and your friends step into the")
+    print("dark asylum,the heavy door creaks shut behind you, echoing through the dimly lit foyer. Dust particales")
+    print("dance in the beams of your flashlights, casting eerie shadows n the peeling walls. The air is thick")
+    print("with the scent o mildew and decay. After a moment of uneasy silence, you turn to yur friends and suggest")
+    print("your next move. Do you want to split into pairs and explore diferent areas or stick together and head")
+    print("to the administrations office?")
+    print("[Pairs]or[Together]")
+    choice = input()
+    if choice == "pairs":
+        go_pairs
+    elif choice == "together":
+        go_together
+def go_together():
+    print("You and yur friends agree that it's best to sta togethe. th group moves cautiously toward the admins office,")
+    print("each step echoing int he eerie silence. The door creaks open, revealing a room shrouded in darkness and dust")
+    print("Do you continue into the Admins office or go back home?")
+    choice = input()
+    if choice =="admins office":
+        go_admin
+    elif choice == "back":
+        go_back
+def go_admin():
+    print("The office is cluttered with old files, overturned furniture, and a thick layer of dust covering")
+    print("everything. As you shine your flashlights around, can't shake the feeling that you're being watched.")
+    print("An old wooden desk stands in the corner, covered in dust. There might be something interesting hidden inside")
+    print("Your friends are torn between checking the desk or going to another room it comes down to your vote. Do you ")
+    print("vote to stay and check the desk or Explore the Main Hall")
+    print("[Desk]or[Main Hall]")
+    choice = input()
+    if choice == "desk":
+        explore_desk
+    elif choice == "main hall":
+        explore_main_hall
+
+
+
+
+
+
+
 
 #Start Program
 main()
