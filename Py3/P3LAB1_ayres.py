a2ef40b39c29bc08ddd71399b5affac9c5eeb694
SarahNicole8025 <ayress3884@student.faytechcc.edu>
2024-10-24 17:12:40 +0000

    Stay Home

--- Py3/P3LAB1_ayres.py
+++ Py3/P3LAB1_ayres.py
@@ -9,13 +9,21 @@ def main():
     print("Come on! We're going to the old ayslum! You must come. It'll be an adventure!")
     print("You glance outside at the darkening clouded sky and feel curiosity as well as apprehension")
     print("What will you do?")
-    print("[asylum] or [home]")
+    print("[home] or [asylum]")
     choice = input()
-    if choice == "Go to the Asylum":
-        go_asylum()
-    elif choice == "Stay home":
+    if choice == "Stay home":
         stay_home()
-
+    elif choice == "Go to the Asylum":
+        go_asylum()
+def stay_home():
+    print("You glance at the flickering TV and the takeout menu spread out on the coffee table.")
+    print("The thought of exploring a creepy, abandoned asylum sends chills down your spine.")
+    print("You can alreay inamgine the shadows lurking in every corner, Your friends can tell from")
+    print("your face what your answer is. So, they groan but ultimately head out without you. You")
+    print("pullout your phone, order a pizza, and settle back into the comfort of your blankey,")
+    print("relieved to be in your safe space.")
+    print("Yay! Your Safe, but you find out in the morning on the news that they were all killed by a ")
+    print("axe whilding Masked figure that has been terrizing the town as of late.")
 
 #Start Program
 main()
